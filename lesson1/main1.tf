de3542207d121b95444028f425cccbbf6a21031f
Akgun <dikkanakgun@gmail.com>
2022-07-25 16:24:36 +0300

    1. lesson added.

---
+++ lesson1/main1.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "tf-ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  key_name      = "firstkey"    # write your pem file without .pem extension>
+  tags = {
+    "Name" = "tf-ec2"
+  }
+}
+
+resource "aws_s3_bucket" "tf-s3" {
+  bucket = "akgun-tf-test-bucket-addwhateveryouwant"
+}
